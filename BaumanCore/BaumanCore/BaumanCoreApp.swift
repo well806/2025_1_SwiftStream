959232cc8330da396c0f1753f4e758c430395722
Kristina <glyzinakristina@gmail.com>
2025-11-19 13:53:27 +0300

    added new
    "new commit"

--- BaumanCore/BaumanCore/BaumanCoreApp.swift
+++ BaumanCore/BaumanCore/BaumanCoreApp.swift
@@ -1,10 +1,3 @@
-//
-//  BaumanCoreApp.swift
-//  BaumanCore
-//
-//  Created by Kristina on 15.11.2025.
-//
-
 import SwiftUI
 
 @main
--- Core/Models/Student.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct Student {
-    var name: String
-    var faculty: String
-    var group: String
-    var studentID: String
-    var email: String
-}
